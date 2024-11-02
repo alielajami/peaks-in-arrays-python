8fc7689c9c426b1506710a7b5fde5fd8d3c289ba
alielajami <alielajami@outlook.com>
2024-11-02 20:17:14 +0200

    commenting for clean code

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 ''' Peaks in arrays application '''
 
-array_size = int(input("Enter the size of the array: "))
-integers_array = []
+array_size = int(input("Enter the size of the array: ")) # Size of the array
+integers_array = [] # Array to store integers
 
-for i in range(array_size):
-    integers_array.append(int(input(f"Enter the integer {i + 1}: ")))
+for i in range(array_size): # Loop to get integers from user
+    integers_array.append(int(input(f"Enter the integer {i + 1}: "))) # Append integers to the array
 
 def find_peaks(array):
     ''' Function to find peaks in an array '''
-    peaks = []
-    for j in range(1, len(array) - 1):
-        if array[j] > array[j - 1] and array[j] > array[j + 1]:
-            peaks.append(array[j])
-    return peaks
+    peaks = [] # List to store peaks
+    for j in range(1, len(array) - 1): # Loop to find peaks
+        if array[j] > array[j - 1] and array[j] > array[j + 1]: # Check if the element is greater than its neighbours
+            peaks.append(array[j]) # Append the element to the peaks list
+    return peaks # Return the peaks list
 
 print(f"There are {len(find_peaks(integers_array))} peaks in the array.")
 print(f"Peaks in the array: {find_peaks(integers_array)}")
